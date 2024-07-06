aa5bafd203fda2ba9eed84a6081a98c65a8d349c
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-07-06 09:05:40 +0530

    LAB_6_4.java

--- LAB_6_4.java
+++ LAB_6_4.java
@@ -1,23 +1,44 @@
+
+// package  DS_PROGRAMES;
 import java.util.Scanner;
 
 public class LAB_6_4 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Fahrenheitcelsiuscalculator fcc = new Fahrenheitcelsiuscalculator();
+        System.out.println(" enetr what you want to convert celsius to fahrenheit  than enter 1 otherwise 0 for fahrenheit to celsius :");
+        int temp = sc.nextInt();
+        if (temp == 1) {
+            fcc.fahrenheitcalculation();
+        } else if (temp == 0) {
+            fcc.celsiuscalculation();
+        } else {
+            System.out.println("enter the correct value");
+        }
     }
 }
 
-class Fahrenheit {
+class Fahrenheitcelsiuscalculator {
+
     double fahrenheit;
     double celsius;
 
     public void celsiuscalculation() {
         Scanner sc = new Scanner(System.in);
-        celsius = sc.nextDouble();
-        fahrenheit = ((celsius - 32) * (5)) / 9;
+        Fahrenheitcelsiuscalculator fcc = new Fahrenheitcelsiuscalculator();
+        System.out.println("enter fahrenheit");
+        fahrenheit = sc.nextDouble();
+        celsius = ((fahrenheit - 32) * (5)) / 9;
+        System.out.println(" your celsius is :" + celsius);
     }
+
     public void fahrenheitcalculation() {
         Scanner sc = new Scanner(System.in);
-        fahrenheit = sc.nextDouble();
-        celsius = ((celsius - 32) * (5)) / 9;
+        Fahrenheitcelsiuscalculator fcc = new Fahrenheitcelsiuscalculator();
+        System.out.println("enter celsius");
+        celsius = sc.nextDouble();
+        fahrenheit = ((celsius * 9) / 5) + 32;
+        System.out.println(" your fahrenheit is :" + fahrenheit);
     }
 }
